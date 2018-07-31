70b7daa44702e9c13a93fac7a0cf62f6f75b75fb
dongjiajun <334631025@qq.com>
2018-07-31 14:05:49 +0800

    1

--- goods/src/main/java/com/dreawer/goods/GoodsApplication.java
+++ goods/src/main/java/com/dreawer/goods/GoodsApplication.java
@@ -2,7 +2,12 @@ package com.dreawer.goods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import zipkin.server.EnableZipkinServer;
+
+@EnableDiscoveryClient   
+@EnableZipkinServer
 @SpringBootApplication
 public class GoodsApplication {
 
