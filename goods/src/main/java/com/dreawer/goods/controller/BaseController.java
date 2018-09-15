5ee5f5a9cb826a344a2425377146bf4dd4b8d16d
dongjiajun <334631025@qq.com>
2018-09-15 12:12:53 +0800

    1

--- goods/src/main/java/com/dreawer/goods/controller/BaseController.java
+++ goods/src/main/java/com/dreawer/goods/controller/BaseController.java
@@ -5,10 +5,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
-import java.util.regex.Pattern;
-
-import org.junit.Test;
-
 import com.dreawer.goods.domain.GoodsPropertyName;
 import com.dreawer.goods.domain.Sku;
 
